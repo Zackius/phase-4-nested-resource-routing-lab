fab0cfb11994ef62d3f0e6d4dba789ae294ea069
Zacharia <ndunguzachary24@gmail.com>
2022-10-06 15:44:47 +0300

    commit tree 411e841eb4e5c7c390fd2cbd870167dc2b09254b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user =  User.find(params[:user_id])
+      items = user.items
+    else
+items = item.all
+    end
+    render json: items, include:  :user
   end
 
+  def show 
+item = find_item
+render json: item 
+  end
+
+  def create
+user =  User.find(params[:user_id])
+item = user.items.create(item_params)
+render json:  item, status: :created
+  end 
+
+
+
+  private 
+
+  def find_item
+Item.find(params[:id])
+  end
+def item_params
+params.permit(:name, :description, :price)
+end
+def render_not_found_response(invalid) 
+render json: {error: "#{invalid.model} not found" }, status:  :not_found
+end
+  
 end
